ffe1c509c8a99175eeea2e1b10c8a2805330fde0
Alexandre Ramos <alexandreramos@MacBook-Pro-de-Alexandre.local>
2023-05-14 20:42:34 -0300

    hardhat config and deploy script done

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,27 +1,32 @@
+import * as dotenv from "dotenv";
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
   networks: {
     mumbai: {
-      url: "",
-      accounts: [],
+      url: process.env.MUMBAI_URL,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
-    token: process.env.REPORT_GAS ? "ETH" : undefined,
+    token: process.env.REPORT_GAS ? "MATIC" : undefined,
     coinmarketcap: process.env.REPORT_GAS
       ? process.env.COIN_MARKET_CAP
       : undefined,
   },
-  // etherscan: {
-  //   apiKey: {
-  //     mumbai: process.env.ETHERSCAN_API_KEY,
-  //   },
-  // },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.ETHERSCAN_API_KEY || "",
+    },
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,18 +1,20 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const Implementation = await ethers.getContractFactory(
+    "CreditDelegationVault"
+  );
+  const impl = await Implementation.deploy();
+  await impl.deployed();
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
+  const CDVFactory = await ethers.getContractFactory(
+    "CreditDelegationVaultFactory"
+  );
+  const cdvFactory = await CDVFactory.deploy(impl.address);
+  await cdvFactory.deployed();
 
   console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    `Implementation deployed to ${impl.address} and CDVFactory deployed to ${cdvFactory.address}`
   );
 }
 
