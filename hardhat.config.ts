f951d03aed53cc9a1fdfbf5bd8daf7ba1b3c226c
Alaa Saleh <alaaisale7@gmail.com>
2024-04-22 17:02:25 -0300

    replace mumbai testnet with seepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,8 +8,8 @@ dotenv.config();
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
   networks: {
-    mumbai: {
-      url: process.env.MUMBAI_URL,
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
@@ -17,14 +17,14 @@ const config: HardhatUserConfig = {
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
-    token: process.env.REPORT_GAS ? "MATIC" : undefined,
+    token: process.env.REPORT_GAS ? "ETH" : undefined,
     coinmarketcap: process.env.REPORT_GAS
       ? process.env.COIN_MARKET_CAP
       : undefined,
   },
   etherscan: {
     apiKey: {
-      polygonMumbai: process.env.ETHERSCAN_API_KEY || "",
+      sepolia: process.env.ETHERSCAN_API_KEY || "",
     },
   },
 };
